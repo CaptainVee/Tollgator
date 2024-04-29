8332dd7069c08e1b35ad2418b84400d61b6aa6b4
CaptainVee <captainvee3@gmail.com>
2024-04-29 16:23:47 +0100

    removed get_env for secrets.json

--- get_env.py
+++ get_env.py
@@ -2,17 +2,17 @@ import json
 
 # Normally you should not import ANYTHING from Django directly
 # into your settings, but ImproperlyConfigured is an exception.
-from django.core.exceptions import ImproperlyConfigured
+# from django.core.exceptions import ImproperlyConfigured
 
-# JSON-based secrets module
-with open("secrets.json") as f:
-    secrets = json.load(f)
+# # JSON-based secrets module
+# with open("secrets.json") as f:
+#     secrets = json.load(f)
 
 
-def get_secret(setting, secrets=secrets):
-    """Get the secret variable or return explicit exception."""
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = f"Set the {setting} environment, variable"
-    raise ImproperlyConfigured(error_msg)
+# def get_secret(setting, secrets=secrets):
+#     """Get the secret variable or return explicit exception."""
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         error_msg = f"Set the {setting} environment, variable"
+#     raise ImproperlyConfigured(error_msg)
